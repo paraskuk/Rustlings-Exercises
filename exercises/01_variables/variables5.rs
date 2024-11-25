df1cb5cb76343ca6d2c3821bcb70070e9356c4de
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-11-25 20:01:35 +0000

    Solve Chapter 1 variables

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,6 +3,15 @@ fn main() {
     println!("Spell a number: {}", number);
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    // how to cast in rust show me an example how I can cast the number to an integer write code
+
+
+    let number = 3;
     println!("Number plus two is: {}", number + 2);
 }
+//
+// fn main() {
+//     let number_str = "3";
+//     let number: i32 = number_str.parse().expect("Not a number!");
+//     println!("Number plus two is: {}", number + 2);
+// }
