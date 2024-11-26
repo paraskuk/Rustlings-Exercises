c304019b1d922cc0beaaba89bac26741a983437d
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-11-26 22:02:48 +0000

    move semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,9 +1,7 @@
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
-    vec.push(88);
-
-    vec
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut new_vec = vec.clone();
+    new_vec.push(88);
+    new_vec
 }
 
 fn main() {
@@ -20,9 +18,9 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(&vec0);
 
-        assert_eq!(vec0, [22, 44, 66]);
-        assert_eq!(vec1, [22, 44, 66, 88]);
+        assert_eq!(vec0, vec![22, 44, 66]);
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
     }
 }
