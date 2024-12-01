97e6ce06ec4aa74cddf7c65b09f30d07878efe72
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-12-01 18:49:59 +0000

    strings

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -44,8 +44,6 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message
-        // variants using the methods defined above.
         match message {
             Message::Resize { width, height } => self.resize(width, height),
             Message::Move(point) => self.move_position(point),
@@ -64,16 +62,20 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_match_message_call() {
-        let mut state = State {
+    fn create_initial_state() -> State {
+        State {
             width: 0,
             height: 0,
             position: Point { x: 0, y: 0 },
             message: String::from("hello world"),
             color: (0, 0, 0),
             quit: false,
-        };
+        }
+    }
+
+    #[test]
+    fn test_match_message_call() {
+        let mut state = create_initial_state();
 
         state.process(Message::Resize {
             width: 10,
